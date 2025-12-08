880423352c3c3572bc8150f151fa39bfb5cff9f6
Piratebird <allenandersonuwu@gmail.com>
2025-12-09 01:24:51 +0200

    torrent filter simple logic

--- core/torrent_filter.py
+++ core/torrent_filter.py
@@ -1,3 +1,14 @@
-'''
-
 """
+filter torrents functionality based on seeds and quality
+
+core/torrent_filter.py
+"""
+
+
+def match_quality(title, qualities):
+    title = title.lower()
+    return any(q.lower() in title for q in qualities)
+
+
+def dedupe(items):
+    pass
